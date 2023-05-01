63c8cfd1e9d2bf36e0b9de4c7b3dd161777ecb61
mon842 <sayakbasak842@gmail.com>
2023-05-01 23:07:48 +0530

    radial gradient added on tracks

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Black_Ops_One } from 'next/font/google';
 import Navbar from '@/Components/Navbar/Navbar';
 import Banner from '@/Components/Banner/Banner';
 import About from '@/Components/About/About';
@@ -9,17 +8,14 @@ import Contact from "@/Components/Contact/contact"
 import Footer from "@/Components/Footer/Footer";
 import TracksCard from '@/Components/Tracks/TracksCard';
 import Counts from '@/Components/Count/Counts';
-const blackopsone = Black_Ops_One({
-  subsets: ['latin'],
-  weight: ['400']
-})
+
 
 
 const index = () => {
 
   return (
 
-    <div className={blackopsone.className}>
+    <div >
       {/* Navbar */}
       <Navbar />
 
